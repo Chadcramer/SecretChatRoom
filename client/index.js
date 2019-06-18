09d63f9864e482c31cf204e2649af7b82120de41
Chad Cramer <chcr6493@colorado.edu>
2019-06-17 20:58:54 -0500

    sdf

--- client/index.js
+++ client/index.js
@@ -1,5 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './Components/App.jsx'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './Components/App.jsx'
+const fs = require('fs');
+var Jimp = require('jimp');
 
-ReactDOM.render(< App />, document.getElementById('root'));
+
+Jimp.read("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/757px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg", function (err, image) {
+    // image.resize(200, 200);
+    // image.quality(60);
+    for(let i = 0; i < 756; i++){
+        for(let j = 0; j < 599; j ++){
+            // fs.appendFileSync('../data/picData.txt', Object.values(Jimp.intToRGBA(image.getPixelColor(j, i))))
+            console.log(Jimp.intToRGBA(image.getPixelColor(j, i))); // returns the colour of that pixel e.g. 0xFFFFFFFF
+        }
+    }
+});
+
+
+// ReactDOM.render(< App />, document.getElementById('root'));
