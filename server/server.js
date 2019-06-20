66b217133219c9b69632b01049ed4f8d972f3a09
Chad Cramer <chcr6493@colorado.edu>
2019-06-20 18:28:44 -0500

    ready for presentation

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const app = express();
 
 // comment out one or the other to show specific image 
 const { colorArr } = require('./imgConverter');
-// const { colorArr } = require('./image3');
 // const { colorArr } = require('./image2');
+// const { colorArr } = require('./image3');
 
 const port = 3000;
 
