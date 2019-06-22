c412bdd0f23fa6dfda96285d0ea0e2098f8af047
Chad Cramer <chcr6493@colorado.edu>
2019-06-22 15:59:18 -0500

    comment

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// comment out one or the other to show specific image 
-const { colorArr } = require('./imgConverter');
-// const { colorArr } = require('./image2');
+// comment out one or the other to show specific image
+// const { colorArr } = require('./imgConverter');
+const { colorArr } = require("./image2");
 // const { colorArr } = require('./image3');
 
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get('/image', (req, res) => {
-    res.json(colorArr);
-})
+app.get("/image", (req, res) => {
+  res.json(colorArr);
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Listen
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
